2c962ba9a71e79f97084b7a5504497171f781df0
Moe Lenge <Moe Lenge>
2017-04-15 15:41:49 -0500

    changing routes order

--- routes/orders.js
+++ routes/orders.js
@@ -11,12 +11,15 @@ const db = mongojs('mongodb://localhost:27017/liquidNitro');
 router.post('/saverorder', function(req, res, next)
 {
 
-    console.log(req.body.order);
+    console.log(req.body);
    
     db.order.insert({
-        "orderNumber":101,
-        "userId":105,
-        "products": req.body.order
+        "orderNumber":req.body.orderNumber,
+        "date": new Date(),
+        "Customer": req.body.userID.fName + req.body.userID.lName,
+        "userId":req.body.userID._id,
+        "products": req.body.order,
+        "Total" : req.body.totalSum
                    
     })
 
@@ -24,8 +27,20 @@ router.post('/saverorder', function(req, res, next)
         success: true
     })
 
-})
+}) ;
 
+router.get('/orderNumber', function(req,res,next)
+{
+    db.order.findOne({_id:mongojs.ObjectId('58f26c8ef705943bcc243e5c')},function(err,orderNumber){
+        if(err){
+            res.send(err)
+        }
+        res.send(orderNumber);
+    })
+
+    db.order.update({_id:mongojs.ObjectId('58f26c8ef705943bcc243e5c')}, {$inc:{orderNumber:1}});
+
+});
 router.get('/orders',function(req,res,next)
 {
  
